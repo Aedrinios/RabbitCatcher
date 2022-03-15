cfcc847872c5696ea2644bfb19b9d61006f85736
Aedrinios <clementclerc2@gmail.com>
2022-03-15 11:53:03 +0100

    reboot project

--- TwinStickCPP/Source/TwinStickCPP/TwinStickCPP.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "TwinStickCPP.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TwinStickCPP, "TwinStickCPP" );
-
-DEFINE_LOG_CATEGORY(LogTwinStickCPP)
- 
